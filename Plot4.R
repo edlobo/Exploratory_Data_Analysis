5c2971e2fc7c393d4766fdfefd4160f175985b1b
edlobo <morolobo@gmail.com>
2016-01-10 01:50:04 -0300

    Loading first assignment

--- Plot4.R
+++ Plot4.R
@@ -15,12 +15,13 @@
 # Getting Plot4
 
         library(lubridate)
+        png("Plot4.png", width = 480, height = 480)
         par(mfrow=c(2,2))
         fecha <- dmy(DataIn$Date)
         DataIn$Period <- NULL
         # Formatting data into a valid class and creating a new columm in the data frame
         DataIn$Period<- ymd_hms(paste(fecha, DataIn$Time, sep = " "))
-
+    
         # First plot
         with(DataIn, plot(Period,Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="") )
 
@@ -31,7 +32,9 @@
         with(DataIn, plot(Period, Sub_metering_1, ylab ="Energy Sub Metering", xlab ="", type = "l"))
         with(DataIn, lines(Period, Sub_metering_2, type = "l", col="red"))
         with(DataIn, lines(Period, Sub_metering_3, type = "l", col="blue"))
-        legend("topright", lty=1, legend=c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), col = c("black", "red", "blue"), bty = "n", cex = 0.399, xjust=1, y.intersp = 0.4)
+        legend("topright", lty=1, legend=c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), col = c("black", "red", "blue"), bty = "n", cex = 0.5, xjust=1, y.intersp = 0.4)
 
         # Four Plot
         with(DataIn, plot(Period,Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime") )
+        dev.off()
+        
